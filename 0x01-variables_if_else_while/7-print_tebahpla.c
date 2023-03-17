1dccbeaefd4785290fc05b656b11d3ea0499fb4d
Bolarinwa Bisola <bolarinwabisola@yahoo.com>
2023-03-17 07:10:22 -0700

    not sure

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,16 +1,16 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include "stdio.h"
+#include "stdlib.h"
+#include "time.h"
 /**
- * main - entry point
+ * main - Entry point
  * Return: Always 0
  */
 int main(void)
 {
-	char low;
+	char i;
 
-	for (low <= 'z'; low = 'a'; low--)
-	putchar(low);
+	for (i = 'z'; i >= 'a'; i--)
+	putchar(i);
 	putchar('\n');
 	return (0);
-	}
+}
