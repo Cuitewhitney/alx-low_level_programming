aec371a417db10392dfb8dadc8372f8472d5524a
Bolarinwa Bisola <bolarinwabisola@yahoo.com>
2023-04-17 14:03:58 -0700

    Update all

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,12 +1,3 @@
-#include <stdio.h>
-#ifdef OBJECT_LIKE_MACRO_H
-
-/**
- * main - define a macro to be SIZE
- * give it an abbreviation of token 1024
- * 
- * Return: Always 0 (Success)
- */
-
+#ifndef SIZE
 #define SIZE 1024
 #endif
