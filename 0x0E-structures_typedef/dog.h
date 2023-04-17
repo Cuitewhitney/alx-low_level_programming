4c488a58b5834d3d6895aa144ede698cb8765457
Bolarinwa Bisola <bolarinwabisola@yahoo.com>
2023-04-17 15:11:41 -0700

    All

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,24 +2,20 @@
 #define DOG_H
 
 /**
- * struct dog - a dog's short write up
- * @name: The dog's name
- * @age: The dog's age
- * @owner: The dog's owner
+ * struct dog - a short note
+ * @name: The Dog's name
+ * @age: The Dog's age
+ * @owner: The Dog's owner
  *
  * Description: Longer description
  */
 
-struct dog 
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-
-/**
- * dog_t - typedef for struct dog
- */
 
 typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
