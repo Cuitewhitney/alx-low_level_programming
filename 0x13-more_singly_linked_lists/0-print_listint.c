3e5b3b4f619cf9b2e074324ee440da3189c3d995
Bolarinwa Bisola <bolarinwabisola@yahoo.com>
2023-04-29 14:29:25 -0700

    New

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,7 +1,8 @@
 #include "lists.h"
+#include <stdio.h>
 
 /**
- * print_listint - prints all the elements of a linked list
+ * print_listint - This prints all the elements of a linked list
  * @h: linked list of type listint_t to print
  *
  * Return: number of nodes
