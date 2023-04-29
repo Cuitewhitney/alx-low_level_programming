5590d2fb4cbab2aab3884e8cb819e77cf80d69ac
Bolarinwa Bisola <bolarinwabisola@yahoo.com>
2023-04-29 14:14:34 -0700

    New

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,22 +1,22 @@
-#include "lists.h"
 #include <stdio.h>
+#include "lists.h"
 
 /**
  * print_listint - This prints all the elements of a linked list
  * @h: This is the linked list of type listint_t to print
  *
- * Return: This equals to the  number of nodes
+ * Return: The number of nodes
  */
-
 size_t print_listint(const listint_t *h)
 {
-	size_t nodes = 0;
+	size_t sim = 0;
 
-	while (h != NULL)
+	while (h)
 	{
 		printf("%d\n", h->n);
+		sim++;
 		h = h->next;
-		nodes++;
 	}
-	return (nodes);
+
+	return (sim);
 }
--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,11 +1,12 @@
+#include <stdio.h>
 #include "lists.h"
 
 /**
- * delete_nodeint_at_index - This deletes a node in a linked list at a certain index
- * @head: This is the pointer to the first element in the list
- * @index: This is the index of the node to delete
+ * delete_nodeint_at_index - deletes a node in a linked list at a certain index
+ * @head: pointer to the first element in the list
+ * @index: index of the node to delete
  *
- * Return: if sucessful return 1 else return -1 if failed
+ * Return: 1 (Success), or -1 (Fail)
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
