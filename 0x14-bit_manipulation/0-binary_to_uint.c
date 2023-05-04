4683ba4c05f2bcfa77a3491827b2d9d93da3424d
Bolarinwa Bisola <bolarinwabisola@yahoo.com>
2023-05-04 06:57:20 -0700

    Bit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * binary_to_uints - This converts a binary number to an unsigned integer
+ * binary_to_uint - This converts a binary number to an unsigned integer
  * @b: This is the string containing the binary number
  *
  * Return: The return function returns the converted number
@@ -21,6 +21,5 @@ unsigned int binary_to_uint(const char *b)
 			return (0);
 		my_val = 2 * my_val + (b[a] - '0');
 	}
-
 	return (my_val);
 }
